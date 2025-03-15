9c5dec6ba597f44343c383985f1695604b45f09e
bieniu <mikibien@outlook.com>
2025-03-15 18:56:22 +0100

    fix: correct app

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,30 +2,27 @@ import type { Metadata } from "next";
 import { Lato } from "next/font/google";
 import "./globals.css";
 import Spotlight from "@/components/ui/Spotlight";
-import { MainContextProvider } from "@/components/Context";
 
 const lato = Lato({ subsets: ["latin-ext"], weight: ["400", "700", "900"] });
 
 export const metadata: Metadata = {
-  title: "Mikolaj Bien",
-  description: "junior front-end dev",
+	title: "Mikolaj Bien",
+	description: "junior front-end dev",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html className="scroll-smooth" lang="en">
-      <body className={lato.className}>
-        <MainContextProvider defaultValue="about">
-          <div className={"relative min-h-screen bg-slate-900 text-slate-200"}>
-            <Spotlight className="fixed inset-0" />
-            {children}
-          </div>
-        </MainContextProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html className="scroll-smooth" lang="en">
+			<body className={lato.className}>
+				<div className={"relative min-h-screen bg-slate-900 text-slate-200"}>
+					<Spotlight className="fixed inset-0" />
+					{children}
+				</div>
+			</body>
+		</html>
+	);
 }
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,67 +1,47 @@
 "use client";
 import { twMerge } from "tailwind-merge";
-import { useMainContext } from "./Context";
 
+type Link = { href: `${"#" | "https://"}${string}`; text: string };
+const links: Link[] = [
+	{
+		href: "#about",
+		text: "ABOUT",
+	},
+	{
+		href: "#projects",
+		text: "PROJECTS",
+	},
+	{
+		href: "#contacts",
+		text: "CONTACTS",
+	},
+];
 export default function Nav({ className }: { className?: string }) {
-  // const { focus } = useMainContext();
-  return (
-    <nav className={twMerge(className)}>
-      <ul className="w-max">
-        <li>
-          <a href="#about" className="group flex flex-row items-center py-2">
-            <span
-              className={twMerge(
-                "inline-block h-px w-9 bg-slate-600 mr-4 transition-all group-active:bg-slate-100  group-active:w-14 group-hover:bg-slate-400 group-hover:w-14",
-                //               focus === "about" ? "bg-slate-100  w-14" : "",
-              )}
-            ></span>
-            <span
-              className={twMerge(
-                "text-slate-500 font-black transition-all text-md group-hover:text-slate-300 group-active:text-slate-100",
-                //               focus === "about" ? "text-slate-100 " : "",
-              )}
-            >
-              ABOUT
-            </span>
-          </a>
-        </li>
-        <li>
-          <a href="#projects" className="group flex flex-row items-center py-2">
-            <span
-              className={twMerge(
-                "inline-block h-px w-9 bg-slate-600 mr-4 transition-all group-active:bg-slate-100  group-active:w-14 group-hover:bg-slate-400 group-hover:w-14",
-                //              focus === "projects" ? "bg-slate-100  w-14" : "",
-              )}
-            ></span>
-            <span
-              className={twMerge(
-                "text-slate-500 font-black transition-all text-md group-hover:text-slate-300 group-active:text-slate-100",
-                //               focus === "projects" ? "text-slate-100 " : "",
-              )}
-            >
-              PROJECTS
-            </span>
-          </a>
-        </li>
-        <li>
-          <a href="#contacts" className="group flex flex-row items-center py-2">
-            <span
-              className={twMerge(
-                "inline-block h-px w-9 bg-slate-600 mr-4 transition-all group-active:bg-slate-100  group-active:w-14 group-hover:bg-slate-400 group-hover:w-14",
-                //               focus === "contacts" ? "bg-slate-100  w-14" : "",
-              )}
-            ></span>
-            <span
-              className={twMerge(
-                "text-slate-500 font-black transition-all text-md group-hover:text-slate-300 group-active:text-slate-100",
-                //               focus === "contacts" ? "text-slate-100 " : "",
-              )}
-            >
-              CONTACTS
-            </span>
-          </a>
-        </li>
-      </ul>
-    </nav>
-  );
+	return (
+		<nav className={twMerge(className)}>
+			<ul className="w-max">{links.map(LinkCard)}</ul>
+		</nav>
+	);
+}
+
+function LinkCard(props: Link) {
+	return (
+		<li key={props.href}>
+			<a href={props.href} className="group flex flex-row items-center py-2">
+				<span
+					className={
+						"inline-block h-px w-9 bg-slate-600 mr-4 transition-all group-active:bg-slate-100  group-active:w-14 group-hover:bg-slate-400 group-hover:w-14"
+					}
+				></span>
+				<span
+					className={
+						"text-slate-500 font-black transition-all text-md group-hover:text-slate-300 group-active:text-slate-100"
+						//               focus === "about" ? "text-slate-100 " : "",
+					}
+				>
+					{props.text}
+				</span>
+			</a>
+		</li>
+	);
 }
